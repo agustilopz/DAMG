903c0e16df641517da28c2373710ef2be9335d38
Marc Bonamusa <marc.bonamusa.7e6@itb.cat>
2025-04-10 06:39:09 +0000

    Pagina inicio hecha

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-// Manejo del menú desplegable
-const menuToggle = document.getElementById("menu-toggle");
-const sidebar = document.getElementById("sidebar");
-const closeMenu = document.getElementById("close-menu");
-
-menuToggle.addEventListener("click", () => {
-    sidebar.classList.toggle("active");
-});
-
-closeMenu.addEventListener("click", () => {
-    sidebar.classList.remove("active");
-});
+function toggleMenu() {
+const menu = document.getElementById('sideMenu');
+menu.classList.toggle('active');
+}
